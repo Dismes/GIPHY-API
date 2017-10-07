a8c55087247fec58fc7f309beed5aae663473a13
Yuki Nakano <yuhki.n@gmail.com>
2017-10-07 00:45:02 -0700

    DONE

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -1,32 +1,28 @@
-// Initial array of movies
-var movies = ["Cat", "Dog", "Frog", "Cow"];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+var movies = [];
+
 function displayMovieInfo() {
     var animal = $(this).attr("data-name");
 
-    // Constructing a queryURL using the animal name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    // Performing an AJAX request with the queryURL
     $.ajax({
             url: queryURL,
             method: "GET"
         })
-        // After data comes back from the request
+
         .done(function (response) {
             console.log(queryURL);
 
             console.log(response);
-            // storing the data from the AJAX request in the results variable
+
             var results = response.data;
 
-            // Looping through each result item
             for (var i = 0; i < results.length; i++) {
 
                 var animalDiv = $("<div>");
-                var animalImage = $("<img>");   
+                var animalImage = $("<img class='picture'>");   
                 console.log(results[i].images.fixed_height.url);
                 animalImage.attr("src", results[i].images.fixed_height_still.url);
                 animalImage.attr("data-still", results[i].images.fixed_height_still.url); 
@@ -34,16 +30,12 @@ function displayMovieInfo() {
                 animalImage.attr("data-state", "still");
                 animalImage.attr("class", "gif");
                 animalDiv.append(animalImage);
-                $("#movies-view").prepend(animalDiv);
+                $("#movies-view").prepend(animalImage);
             }
 
             $(".gif").on("click", function() {
                 console.log("Click on gif");
-                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
                 var state = $(this).attr("data-state");
-                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                // Then, set the image's data-state to animate
-                // Else set src to the data-still value
                 if (state === "still") {
                   $(this).attr("src", $(this).attr("data-animate"));
                   $(this).attr("data-state", "animate");
@@ -56,50 +48,35 @@ function displayMovieInfo() {
 
 }
 
-
-
-
-
-// Function for displaying movie data
 function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
 
-    // Looping through the array of movies
     for (var i = 0; i < movies.length; i++) {
 
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class of movie to our button
+    
+        var a = $("<button class='row button btn btn-block' style='padding:auto'>");
+
         a.addClass("movie");
-        // Adding a data-attribute
+
         a.attr("data-name", movies[i]);
-        // Providing the initial button text
+
         a.text(movies[i]);
-        // Adding the button to the buttons-view div
         $("#buttons-view").append(a);
     }
 }
 
-// This function handles events where a movie button is clicked
 $("#add-movie").on("click", function (event) {
     console.log("This button");
     event.preventDefault();
-    // This line grabs the input from the textbox
+
     var movie = $("#movie-input").val().trim();
 
-    // Adding movie from the textbox to our array
     movies.push(movie);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
 });
 
-// Adding a click event listener to all elements with a class of "movie"
 $(document).on("click", ".movie", displayMovieInfo);
 
-// Calling the renderButtons function to display the intial buttons
 renderButtons();
